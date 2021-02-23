cb54adf5c0e42fdddb1298062786b215a4816e3c
User <User@DESKTOP-CA2NBPR>
2021-02-23 10:54:56 +0800

    print case

--- F.CS311-Lab_2/src/Lab2/Lab2.java
+++ F.CS311-Lab_2/src/Lab2/Lab2.java
@@ -7,42 +7,54 @@ import java.util.Stack;
 public class Lab2 {
         
 	private int top;
-	private int capacity;
+	//private int size;
 	private int arr[];
 	
 	
 	
-	
-	      public boolean isEmpty() {
+	//Check is stack empty
+	      public boolean isEmpty()
+	      {
 	    	 return (top == -1);
-	     }
+	      }
+	      
+	      // show top element
 	      public int peek()
 	      {
 	          if (!isEmpty())
 	              return arr[top];
 	          return -1;
 	      }
+	      
+	      // show stack size
 	      public int size()
 	      {
 	          return top + 1;
 	      }
+	      
+	      //pop top element
+	      
 	      public int pop()
 	      {
 	          // check for stack underflow
-	          if (isEmpty())
+	          if (!isEmpty())
 	         
 	           return arr[top--];
 	      }
+	      
+	      
+	     //push element top
 	      public void push(int x)
 	      {
 	          if (!isEmpty())
 	          
 	          arr[++top] = x;
 	      }
-	      public Boolean isFull()
-	      {
-	          return top == capacity - 1;    // or return size() == capacity;
-	      }
+	     
+	     // public boolean isFull()
+	     // {
+	      //    return top == capacity - 1;   
+	    //  }
 	      
 	     
 	   public static void main(String[] args) {
@@ -61,7 +73,7 @@ public class Lab2 {
 			Scanner sc = new Scanner(System.in);
 			
 			System.out.println("============================================MENU============================================");
-			System.out.println("empty\npeek\npush\npop\nsize");
+			System.out.println(" 1.empty\n 2.peek\n 3.push\n 4.pop\n 5.size\n 6.printStack");
 			
 			
 			for(;;) {
@@ -69,9 +81,14 @@ public class Lab2 {
 				String songolt = sc.next();
 				
 				switch(songolt) {
-				case "empty": System.out.println("Is empty: " + mystack.isEmpty()); break;
-				case "peek": System.out.println("Peek element: " + mystack.peek()); break;
-				case "push": {
+				case "empty":
+					System.out.println("Is empty: " + mystack.isEmpty());
+					break;
+				case "peek":
+					System.out.println("Peek element: " + mystack.peek());
+					break;
+				case "push": 
+				{
 					System.out.print("Enter push element: ");
 					try {
 					mystack.push(sc.nextInt());
@@ -81,7 +98,8 @@ public class Lab2 {
 					}
 					break;
 				}
-				case "pop": try {
+				case "pop": 
+					try {
 					System.out.println("Pop: " + mystack.pop());
 					} 
 				catch(Exception e) {
@@ -91,6 +109,12 @@ public class Lab2 {
 				case "size":
 					System.out.println("Size: " + mystack.size()); 
 					break;
+				case "printStack":
+				{
+					Object[] starr = mystack.toArray();
+					for(int i = 0; i < starr.length; i++)
+						System.out.println(starr[i] + "");
+				}
 			    case "exit": 
 					System.exit(0); sc.close(); 
 					break;
